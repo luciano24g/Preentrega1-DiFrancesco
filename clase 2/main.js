55327553f4c884f120cf923200c70ebfb12f6fd1
Luciano Di Francesco <Luchitodf2@gmail.com>
2023-07-12 03:23:32 -0300

    cambios

--- clase 2/main.js
+++
@@ -1,8 +0,0 @@
-let numero = Number 
-(prompt("Ingrese un numero"))
-
-if (numero > 1000) {
-    alert ("El numero ingresador es mayor a 1000")
-    
-}
-else alert("El numero ingresador no es mayor a 1000")
