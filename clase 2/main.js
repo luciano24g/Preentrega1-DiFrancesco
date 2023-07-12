174876632315e1ffd816e27a8494a3889c4e807e
Luciano Di Francesco <Luchitodf2@gmail.com>
2023-07-12 03:20:43 -0300

    primer commit

---
+++ clase 2/main.js
@@ -0,0 +1,8 @@
+let numero = Number 
+(prompt("Ingrese un numero"))
+
+if (numero > 1000) {
+    alert ("El numero ingresador es mayor a 1000")
+    
+}
+else alert("El numero ingresador no es mayor a 1000")
